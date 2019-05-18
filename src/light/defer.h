3fb7a22d112fde5cf3a5949f51ec9d434f845435
Luiz Afonso <lacbs@cin.ufpe.br>
2019-05-18 00:39:27 -0700

    Update defer.h
    Sad: no way for defer_oe?

--- src/light/defer.h
+++ src/light/defer.h
@@ -25,28 +25,59 @@
 // Notes:
 // - There is no "finally" in C++, 
 // - Use extra caution when throwing exception from the defer code, see:
-//   - defer_ufe: Use first exception (default, use first uncaught exception, C++17 behavior)
-//   - defer_ig: Ignore own exception (simply add a try/catch block to the code)
-//   - defer_ule: Use last exception (similar to what python does, use last uncaught exception)
+//   - [can't find a way to implement this] defer_oe: Override Exception (ignore previously uncaught exception if throwing, similar to what python does)
+//   - defer_ie: Ignore own Exception (simply add a try/catch block to the code, "safest")
+//   - defer_iu: Ignore own excpetion if there is an Uncaught one
+//   - defer_te: Throw Exception (default, aborts if there is an uncaught exception and is thowning)
 //   - defer_ne: No exception (aborts if there is an exception, similar to noexcept(true) declaration)
 
+#ifdef __COUNTER__
 #define LIGHT_DEFER_WITH(type, code) light::defer ## type __light_defer_ ## __COUNTER__ ([&] { code; });
-#define LIGHT_DEFER_UFE(code) LIGHT_DEFER_WITH(ufe, code)
-#define LIGHT_DEFER_IG(code) LIGHT_DEFER_WITH(ig, code)
-#define LIGHT_DEFER_ULE(code) LIGHT_DEFER_WITH(ule, code)
+#else
+#define LIGHT_DEFER_WITH(type, code) light::defer ## type __light_defer_ ## __LINE__ ([&] { code; }); // TODO: stringfy line
+#endif
+#define LIGHT_DEFER_OE(code) LIGHT_DEFER_WITH(oe, code)
+#define LIGHT_DEFER_IE(code) LIGHT_DEFER_WITH(ie, code)
+#define LIGHT_DEFER_IU(code) LIGHT_DEFER_WITH(iu, code)
+#define LIGHT_DEFER_TE(code) LIGHT_DEFER_WITH(te, code)
 #define LIGHT_DEFER_NE(code) LIGHT_DEFER_WITH(ne, code)
-#define LIGHT_DEFER(code) LIGHT_DEFER_UFE(code)
+#define LIGHT_DEFER(code) LIGHT_DEFER_TE(code)
 
 namespace light {
 
+bool uncaught_exception() {
+#if __cplusplus >= 201703
+  return std::uncaught_exceptions() > 0;
+#else
+  return std::uncaught_exception();
+#endif
+}
+ 
+ class sentinel
+{
+    int prev_uncaught;
+
+public:
+    sentinel() : prev_uncaught(std::uncaught_exceptions()) {}
+
+    ~sentinel()
+    {
+        int cur_uncaught = std::uncaught_exceptions();
+        if (cur_uncaught > prev_uncaught)
+        {
+        }
+    }
+};
+ 
+ // try { throw; }
 // Use first exception
-class defer_ufe {
+class defer_te {
  public:
   template <typename Func>
   defer_ufe(Func func) {}
  private:
 };
 
-using defer = defer_ufe;
+using defer = defer_te;
 
 }
